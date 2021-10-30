1576ef60044462c9dfbdeebb21f4cdccd812bc15
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-10-30 16:12:29 +0300

    test: fetchAuthTokenRaw

---
+++ tests/FetchAuthTokenRawTest.php
@@ -0,0 +1,69 @@
+<?php
+
+// Copyright (C) 2021 Ivan Stasiuk <brokeyourbike@gmail.com>.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace BrokeYourBike\ZenithBank\Tests;
+
+use Psr\SimpleCache\CacheInterface;
+use Psr\Http\Message\ResponseInterface;
+use BrokeYourBike\ZenithBank\Interfaces\ConfigInterface;
+use BrokeYourBike\ZenithBank\Client;
+
+/**
+ * @author Ivan Stasiuk <brokeyourbike@gmail.com>
+ */
+class FetchAuthTokenRawTest extends TestCase
+{
+    private string $username = 'unique-username';
+    private string $password = 'secure-password';
+
+    /**
+     * @test
+     * @dataProvider isLiveProvider
+     */
+    public function it_can_prepare_request(bool $isLive): void
+    {
+        $mockedConfig = $this->getMockBuilder(ConfigInterface::class)->getMock();
+        $mockedConfig->method('isLive')->willReturn($isLive);
+        $mockedConfig->method('getUrl')->willReturn('https://api.example/');
+        $mockedConfig->method('getUsername')->willReturn($this->username);
+        $mockedConfig->method('getPassword')->willReturn($this->password);
+
+        $mockedResponse = $this->getMockBuilder(ResponseInterface::class)->getMock();
+        $mockedResponse->method('getStatusCode')->willReturn(200);
+        $mockedResponse->method('getBody')->willReturn('{}');
+
+        /** @var \Mockery\MockInterface $mockedClient */
+        $mockedClient = \Mockery::mock(\GuzzleHttp\Client::class);
+        $mockedClient->shouldReceive('request')->withArgs([
+            'POST',
+            'https://api.example/api/authentication/getToken',
+            [
+                \GuzzleHttp\RequestOptions::HTTP_ERRORS => false,
+                \GuzzleHttp\RequestOptions::HEADERS => [
+                    'Accept' => 'application/json',
+                ],
+                \GuzzleHttp\RequestOptions::FORM_PARAMS => [
+                    'userIdentifyer' => $this->username,
+                    'userProtector' => $this->password,
+                ],
+            ],
+        ])->once()->andReturn($mockedResponse);
+
+        $mockedCache = $this->getMockBuilder(CacheInterface::class)->getMock();
+
+        /**
+         * @var ConfigInterface $mockedConfig
+         * @var \GuzzleHttp\Client $mockedClient
+         * @var CacheInterface $mockedCache
+         * */
+        $api = new Client($mockedConfig, $mockedClient, $mockedCache);
+        $requestResult = $api->fetchAuthTokenRaw();
+
+        $this->assertInstanceOf(ResponseInterface::class, $requestResult);
+    }
+}
